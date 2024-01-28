79cd0028b72270c5bfc4df83c8ecd50907533fec
Muhammad Zain Khan <153481211+ZainK07@users.noreply.github.com>
2024-01-28 21:00:30 +0500

    Question No 4.py

---
+++ Question No 4.py
@@ -0,0 +1,15 @@
+# Write a Python program to print the Fibonacci series.
+
+def fibonacci(n):
+    fib_series = [0, 1]
+
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+
+    return fib_series
+
+
+num_terms = 10
+result = fibonacci(num_terms)
+
+print(f"Fibonacci series up to {num_terms} terms: {result}")

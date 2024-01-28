bfd3a0ee7ea1af519ec89fbf2fe7d466388eca1e
Muhammad Zain Khan <153481211+ZainK07@users.noreply.github.com>
2024-01-28 20:57:32 +0500

    Question No 3.py

---
+++ Question No 3.py
@@ -0,0 +1,9 @@
+# Python program to display the sum of n numbers using a list
+
+def sum_of_numbers(numbers):
+    return sum(numbers)
+
+
+user_numbers = [int(x) for x in input("Enter numbers separated by spaces: ").split()]
+result = sum_of_numbers(user_numbers)
+print(f"The sum of the numbers is: {result}")

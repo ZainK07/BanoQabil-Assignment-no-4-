ffa9b50a29629486b9bb626b0aaf7ac2dcddb2a4
Muhammad Zain Khan <153481211+ZainK07@users.noreply.github.com>
2024-01-28 20:54:42 +0500

    Question No 1.py

---
+++ Question No 1.py
@@ -0,0 +1,13 @@
+#Python Program to count unique values inside a list
+
+def a(list):
+    b = set()
+
+    for value in list:
+        b.add(value)
+
+    return len(b)
+
+list1 = [1,2,3,4,2,3,6,7,8,9,10]
+answer = a (list1)
+print("Number of unique values:", answer)
